53ee8f364461b0a90bc723b30f7d07d8b9ed0346
Rafael Santos <devrafaelsantoss@gmail.com>
2025-03-07 00:42:27 -0300

    Adicionado arquivos iniciais

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Adicionando elementos
+const botaoClique = document.querySelector("#clique");
+
+//Adicionando eventos
+botaoClique.addEventListener("click", () => {
+    alert("Funcionou");
+});
+
+//Functions
+
+const esconderBotão = () => {
+    
+}
